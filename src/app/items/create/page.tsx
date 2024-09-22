7aec39c5aeb486858fe890624620eec875e1fdff
Kiril Pashkov <k.pashkov@itransition.com>
2024-09-22 16:53:29 +0200

    Marked component as SSR

--- src/app/items/create/page.tsx
+++ src/app/items/create/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { createItemAction } from "./actions";
@@ -50,36 +48,6 @@ const CreateBid = () => {
         </Button>
       </form>
     </div>
-  );
-
-  return (
-    <>
-      <Heading>Post an item to sell</Heading>
-
-      <form action={createItemAction} className="py-8 rounded-xl space-y-4">
-        <div className="flex flex-col align-items-start gap-x-2 max-w-lg gap-y-4">
-          <Input
-            className=""
-            required
-            name="name"
-            type="text"
-            placeholder="Name your item"
-          />
-          <Input
-            className=""
-            required
-            name="startingPrice"
-            type="number"
-            step="0.01"
-            placeholder="What to start your auction at"
-          />
-          <Input type="file" name="file" required />
-          <Button className="self-end" type="submit">
-            Post item
-          </Button>
-        </div>
-      </form>
-    </>
   );
 };
 
